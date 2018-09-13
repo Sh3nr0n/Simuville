24829fef8cb0fa285ad064804800bf29d06231bf
Sh3nr0n <mehdi.lefeuvre@gmail.com>
2018-09-13 16:31:21 +0200

    implemented population image update for each population interval

---
+++ AbortedDevs/count_elements.js
@@ -0,0 +1,3 @@
+    //Count number of img elements in cityIcons div
+    let countCityImg = document.getElementById("cityIcons"+city.cityId).childElementCount
+    console.log("countCityImg : ",countCityImg)
--- simuvilleml/js/script.js
+++ simuvilleml/js/script.js
@@ -88,37 +88,11 @@ function timeGrowth(maxYear,city){
 
     // Process population value
 
-    pop += pop*city.birthRate-pop*city.deathRate-pop*disasterRate*0.01
-
-    //Count number of img elements in cityIcons div
-    let countCityImg = document.getElementById("cityIcons"+city.cityId).childElementCount
-    console.log("countCityImg : ",countCityImg)
-
-    //Randomize a number between 1 to 36
-
-    // let randomImg = randomizeCity() // Fixed for test purpose
-
-    //A tester
-
-    // let imgList =[
-    //     {cityId: 1, id: "img11", src: "img/31.svg"},
-    //     {cityId: 1, id: "img12", src: "img/36.svg"}
-    // ]
-    // $("#cityIcons"+city.cityId).empty()
-    // imgList.map((img)=>{
-
-    //     //Remove images first
-    //     //html() method does not add new elements in the DOM
-    //     $("#cityIcons"+img.cityId).append("<img id="+img.id+" src='"+img.src+"' height='32' width='32'/>") 
-    //     })
-    
+    pop += pop*city.birthRate-pop*city.deathRate-pop*disasterRate*0.01  
 
     let imgList=city.imgList
-    // console.log("imgList",imgList)
-    // console.log("imgList length",imgList.length)
+    console.log("imageList",imgList)
 
-
-    
     //Prevent population to receive negative values
     if(pop <= 0){ 
         pop = 0
@@ -132,17 +106,13 @@ function timeGrowth(maxYear,city){
         imgList.push(image)
         console.log("imgList:",imgList)
 
-        // Display cities in HTML
-        $("#cityIcons"+city.cityId).empty()
-        console.log("cleaned cities")
-        imgList.map((img)=>{
-        $("#cityIcons"+img.cityId).append("<img id="+img.id+" src='"+img.src+"' height='32' width='32'/>") 
-        })
-
+    }
+    if(pop < 1000 && imgList.length>1 ){
+        imgList.splice(1)
     }
 
     if(pop > 1000 && pop < 10000){
-        let maxBuildings = Math.trunc(pop*0.001)
+        let maxBuildings = Math.trunc(1+ pop*0.001)
         if (maxBuildings>imgList.length){
             console.log("maxBuildings = %s imgList.length= %s",maxBuildings,imgList.length)
 
@@ -160,72 +130,63 @@ function timeGrowth(maxYear,city){
                 imgList.push(image)
                 console.log("imgList:",imgList)
                 }
-        
-            // for (i= imgList.length;i<maxBuildings;i++){
-            // console.log("i = %s , maxBuildings = %s ",i,maxBuildings)
-            // let image = {}
-            // image.cityId = city.cityId
-            // image.id = "img"+city.cityId+i
-            // image.src = "img/"+randomizeCity()+".svg"
-            // imgList.push(image)
-        
-            // console.log("imgList:",imgList)
-            // }
-            // Display cities in HTML
-            $("#cityIcons"+city.cityId).empty()
-            console.log("cleaned cities")
-            imgList.map((img)=>{
-                $("#cityIcons"+img.cityId).append("<img id="+img.id+" src='"+img.src+"' height='32' width='32'/>") 
-                })
-            console.log("Added new random image")
+        }
+
+        if (maxBuildings<imgList.length){
+            console.log("maxBuildings = %s imgList.length= %s",maxBuildings,imgList.length)
+
+            // How many buildings to remove?
+            console.log("there is %s building(s) to remove",imgList.length-maxBuildings)
+            // remove images from array begining at poisition
+            imgList.splice(maxBuildings)
+            console.log("imgList:",imgList)
+
+
         }
     }
 
+    if(pop > 10000){
+        let maxBuildings = Math.trunc(9+pop*0.0001)
+        console.log("maxBuildings = %s imgList.length= %s",maxBuildings,imgList.length)
+        if (maxBuildings>imgList.length){
+            console.log("maxBuildings = %s imgList.length= %s",maxBuildings,imgList.length)
 
-            
-            //Old If (pop > 1000 && pop < 10000){}
-            //Append a new building
-            // $("#cityIcons"+city.cityId).append("<img id='img"+city.cityId+countCityImg+"'src='img/"+randomizeCity()+".svg' height='32' width='32'/>")
-           
+            // How many buildings to add?
+            console.log("there is %s building(s) to add",maxBuildings - imgList.length)
+            // create  as much images as buildings
+            // push new image into array
+            for (i= imgList.length;i<maxBuildings;i++){
+                console.log("i = %s , maxBuildings = %s ",i,maxBuildings)
+                let image = {}
+                image.cityId = city.cityId
+                let newId = imgList.length+1
+                image.id = "img"+city.cityId+newId
+                image.src = "img/"+randomizeCity()+".svg"
+                imgList.push(image)
+                console.log("imgList:",imgList)
+                }
+        }
 
-            // for (i=countCityImg;i=buildingNumber;i++){
-            //     let newId = i+1
-            //     $("#cityIcons"+city.cityId+newId).append("<img id='img"+city.cityId+newId+"'src='img/"+randomizeCity()+".svg' height='32' width='32'/>")
-            //     console.log("Added new random city")
-            // }
+        if (maxBuildings<imgList.length){
+            console.log("maxBuildings = %s imgList.length= %s",maxBuildings,imgList.length)
 
-         
-        
-    //     if(buildingNumber < countCityImg){
-    //         //How many buildings to remove?
-    //         console.log("there is %s buildings to remove",countCityImg - buildingNumber)
-    //         $("#img"+city.cityId+countCityImg).remove();
-    //         console.log("Removed city")
-    //         // removeCount = countCityImg - buildingNumber
-    //         // remove the latest buildings
-    //         // for (i=buildingNumber;i=countCityImg;i--){      
-    //         //     $("#img"+city.cityId+buildingNumber).remove();
-    //         // }
-            
-    //     } 
-    //     // $("#cityIcons"+city.cityId).html("<img src='img/"+randomImg+".svg' height='32' width='32'/>")
-    // }
+            // How many buildings to remove?
+            console.log("there is %s building(s) to remove",imgList.length-maxBuildings)
+            // remove images from array begining at poisition
+            imgList.splice(maxBuildings)
+            console.log("imgList:",imgList)
 
 
+        }
+    }
 
-
-    //To Do : Update HTML with icon image when population reaches predefined intervals
-    //Afficher les images dans un append a chaque fois qu'un seuil de population est atteint
-// Evolution d’affichage :
-// Au fur et à mesure de l’évolution de la population, des images de bâtiment au
-// format svg apparaissent ou disparaissent. Le nombre de bâtiments est un nombre
-// arrondi à l'entier le plus proche.
-// Si la population est inférieure à 1000, il y a 1 bâtiment.
-// Si la population ≤ 10 000, alors il y a 1 bâtiment pour 1000.
-// Lorsque la population > 10 000 alors, il y a 1 bâtiment pour 10 000.
-// Chaque bâtiment affiché sera un bâtiment pris de manière aléatoire parmi les
-// images proposées dans le fchier ico.zip
-// La taille d’une image doit faire 32*32px.
+                // Display cities in HTML
+                $("#cityIcons"+city.cityId).empty()
+                console.log("city icons cleaned")
+                imgList.map((img)=>{
+                    $("#cityIcons"+img.cityId).append("<img id="+img.id+" src='"+img.src+"' height='32' width='32'/>") 
+                    })
+                console.log("city icons updated")
 
     year++
 
@@ -271,7 +232,7 @@ $('#simStart').click(
 
         // Disasters values fixed for test purpose
         city.disasters = {
-            disasterYear:[2,5,150],
+            disasterYear:[2,5,100],
             disasterName:["Eau","Feu","Terre"],
             disasterRate:[5,8,100]
 
